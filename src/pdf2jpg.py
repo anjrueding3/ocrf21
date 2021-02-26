0578240b323768fa4653996a3e0aa9e6bb183444
Andrew Ding <anjrued@gmail.com>
2021-02-26 12:24:54 -0800

    added /src to path

--- src/pdf2jpg.py
+++ src/pdf2jpg.py
@@ -8,7 +8,7 @@ def pdf2jpeg(filePath):
     
     #extract string between last / and .pdf to use as output file name
     outputName = filePath[filePath.rfind('/') + 1 : filePath.rfind('.pdf')] 
-    outputDirectory =  os.getcwd() + '/assets/convertedPDFs'
+    outputDirectory =  os.getcwd() + '/src/assets/convertedPDFs'
    
     #convert pdf to jpg, output to location in assets folder, output as outputName + .jpg
     images = convert_from_path(filePath, dpi = 300, fmt = "jpeg", first_page= 1, single_file=True, output_folder= outputDirectory, output_file= outputName)
